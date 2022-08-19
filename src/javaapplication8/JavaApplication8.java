09313e715580528775d92dc96f2607bb1fad00b1
Omar Loaiza <oloaizaj@gmail.com>
2022-08-19 13:24:43 -0500

    Este proyecto está creado y vacío

--- src/javaapplication8/JavaApplication8.java
+++ src/javaapplication8/JavaApplication8.java
@@ -15,7 +15,7 @@ public class JavaApplication8 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int x=2;
     }
     
 }
