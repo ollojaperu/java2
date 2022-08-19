65bf53d4df6060ba2896ffe6e11ea795eb468fd6
ollojaperu <111599530+ollojaperu@users.noreply.github.com>
2022-08-19 17:54:46 -0500

    Update JavaApplication8.java

--- src/javaapplication8/JavaApplication8.java
+++ src/javaapplication8/JavaApplication8.java
@@ -25,7 +25,7 @@ public class JavaApplication8 {
         //Aqui hay una linea de código comentada
         //Aqui hay una qunta linea de código comentada
         //Aqui hay una sexta linea de código comentada
-        //Aqui hay unaséptima linea de código comentada
+        //Aqui hay una séptima linea de código comentada
     }
     
 }
